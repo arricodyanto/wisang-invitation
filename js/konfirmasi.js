80e94e1c39b8e1632368e44f1c1e9bcdc20d4f03
arricodyanto <arricohandyanto@gmail.com>
2022-09-29 05:36:59 +0700

    fixed

--- js/konfirmasi.js
+++ js/konfirmasi.js
@@ -1,37 +1,30 @@
 //<![CDATA[
 $(document).on('click','.send_form', function(){
-var input_blanter1 = document.getElementById('nama');
-var input_blanter2 = document.getElementById('konfirmasi');
+    var input_blanter1 = document.getElementById('nama');
+    var input_blanter2 = document.getElementById('konfirmasi');
 
-/* Whatsapp Settings */
-var walink = 'https://web.whatsapp.com/send',
-    phone = '6285156637671',
-    header = 'Halo om, saya ',
-    footer = ' ingin mengkonfirmasi bahwa kami *',
-    footer2 = '* dalam acara ulang tahun Wisang. Terimakasih',
-    text_yes = 'Pesan Terkirim.',
-    text_no = 'Harap Isi Semua Form!';
+    /* Whatsapp Settings */
+    var walink = 'https://web.whatsapp.com/send',
+        phone = '6285156637671',
+        header1 = '*KONFIRMASI KEHADIRAN*',
+        header = 'Halo om, saya ',
+        footer = ' ingin mengkonfirmasi bahwa kami *',
+        footer2 = '* dalam acara ulang tahun Wisang. Terimakasih';
 
-/* Smartphone Support */
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-var walink = 'whatsapp://send';
-}
+    /* Smartphone Support */
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    var walink = 'whatsapp://send';
+    }
 
-if("" !=  input_blanter1.value && input_blanter2.value){
+        /* Call Input Form */
+    var input_nama = $("#nama").val(),
+        input_konfirmasi = $("#konfirmasi :selected").text();
 
-    /* Call Input Form */
-var input_nama = $("#nama").val(),
-    input_konfirmasi = $("#konfirmasi :selected").text();
+    /* Final Whatsapp URL */
+    var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + header1 + '%0A%0A' + header
+        + input_nama + footer + input_konfirmasi + footer2 + '%0A';
 
-/* Final Whatsapp URL */
-var blanter_whatsapp = walink + '?phone=' + phone + '&text=' + header1 + '%0A' + header
-    + input_nama + footer + input_konfirmasi + footer2 + '%0A';
-
-/* Whatsapp Window Open */
-window.open(blanter_whatsapp,'_blank');
-document.getElementById("text-info").innerHTML = '<div class="yes alert alert-success">'+text_yes+'</div>';
-} else {
-document.getElementById("text-info").innerHTML = '<div class="no alert alert-warning">'+text_no+'</div>';
-}
+    /* Whatsapp Window Open */
+    window.open(blanter_whatsapp,'_blank');
 });
 //]]>
